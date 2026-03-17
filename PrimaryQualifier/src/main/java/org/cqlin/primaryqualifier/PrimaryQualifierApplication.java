5d1d6658049131537a4942c0e41b54b7fbfbebd5
Lin Changqing <782185366@qq.com>
2026-03-17 10:56:41 +0800

    Create module for display of Qualifier and Primary

---
+++ PrimaryQualifier/src/main/java/org/cqlin/primaryqualifier/PrimaryQualifierApplication.java
@@ -0,0 +1,13 @@
+package org.cqlin.primaryqualifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimaryQualifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimaryQualifierApplication.class, args);
+    }
+
+}
---
+++ PrimaryQualifier/src/test/java/org/cqlin/primaryqualifier/PrimaryQualifierApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cqlin.primaryqualifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaryQualifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
