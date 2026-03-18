19474aab5d9ecef7b00136c5e961bad50a512585
Lin Changqing <782185366@qq.com>
2026-03-18 10:39:47 +0800

    add module of Resource and Autowired

---
+++ ResourceAutowired/src/main/java/org/cqlin/resourceautowired/ResourceAutowiredApplication.java
@@ -0,0 +1,13 @@
+package org.cqlin.resourceautowired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceAutowiredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceAutowiredApplication.class, args);
+    }
+
+}
