86da00a9e072322040c257b6b37030c868db4503
Lin Changqing <782185366@qq.com>
2026-03-19 10:41:44 +0800

    add module of injection methods

---
+++ InjectionDemo/src/main/java/org/cqlin/injectiondemo/InjectionDemoApplication.java
@@ -0,0 +1,13 @@
+package org.cqlin.injectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InjectionDemoApplication.class, args);
+    }
+
+}
