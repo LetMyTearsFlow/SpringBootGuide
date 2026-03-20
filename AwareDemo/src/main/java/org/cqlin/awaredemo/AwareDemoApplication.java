1730387a150fbc3204a27c5b7a1d0ed6bad26a6d
Lin Changqing <782185366@qq.com>
2026-03-20 10:52:59 +0800

    add module of aware interfaces

---
+++ AwareDemo/src/main/java/org/cqlin/awaredemo/AwareDemoApplication.java
@@ -0,0 +1,13 @@
+package org.cqlin.awaredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwareDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwareDemoApplication.class, args);
+    }
+
+}
---
+++ AwareDemo/src/test/java/org/cqlin/awaredemo/AwareDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cqlin.awaredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwareDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
