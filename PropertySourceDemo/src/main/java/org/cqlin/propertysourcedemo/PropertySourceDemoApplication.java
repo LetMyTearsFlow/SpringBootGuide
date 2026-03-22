52e0fdd2915c9d0e0805652e91029918c41cf1b9
Lin Changqing <cq_lin@foxmail.com>
2026-03-22 10:24:49 +0800

    feat: add module of usage @PropertySource

---
+++ PropertySourceDemo/src/main/java/org/cqlin/propertysourcedemo/PropertySourceDemoApplication.java
@@ -0,0 +1,13 @@
+package org.cqlin.propertysourcedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertySourceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertySourceDemoApplication.class, args);
+    }
+
+}
---
+++ PropertySourceDemo/src/test/java/org/cqlin/propertysourcedemo/PropertySourceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cqlin.propertysourcedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertySourceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
