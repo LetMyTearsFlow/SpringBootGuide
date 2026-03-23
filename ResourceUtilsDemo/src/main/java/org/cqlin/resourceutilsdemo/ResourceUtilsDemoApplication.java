544e23ce7bd98e5ec27d1916ca0efc00bd591625
Lin Changqing <cq_lin@foxmail.com>
2026-03-23 10:36:34 +0800

    feat: add module of usage of ResourceUtils.

---
+++ ResourceUtilsDemo/src/main/java/org/cqlin/resourceutilsdemo/ResourceUtilsDemoApplication.java
@@ -0,0 +1,13 @@
+package org.cqlin.resourceutilsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceUtilsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceUtilsDemoApplication.class, args);
+    }
+
+}
