63c3bca3631a6cd65285407eb075ac4e9ff1dae7
Lin Changqing <cq_lin@foxmail.com>
2026-03-28 10:57:17 +0800

    feat: add module of demonstration of bean's life cycle.

---
+++ BeanLifeCycleDemo/src/main/java/org/cqlin/beanlifecycledemo/BeanLifeCycleDemoApplication.java
@@ -0,0 +1,13 @@
+package org.cqlin.beanlifecycledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanLifeCycleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanLifeCycleDemoApplication.class, args);
+    }
+
+}
---
+++ BeanLifeCycleDemo/src/test/java/org/cqlin/beanlifecycledemo/BeanLifeCycleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cqlin.beanlifecycledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanLifeCycleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
