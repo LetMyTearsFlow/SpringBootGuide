7b5b3d7e81f039495618881b71ddd2a2055e2c62
Lin Changqing <cq_lin@foxmail.com>
2026-03-30 09:47:56 +0800

    feat: add a module to show the usage of static proxy.

---
+++ StaticProxyDemo/src/main/java/org/cqlin/staticproxydemo/StaticProxyDemoApplication.java
@@ -0,0 +1,13 @@
+package org.cqlin.staticproxydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticProxyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaticProxyDemoApplication.class, args);
+    }
+
+}
---
+++ StaticProxyDemo/src/test/java/org/cqlin/staticproxydemo/StaticProxyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cqlin.staticproxydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticProxyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
