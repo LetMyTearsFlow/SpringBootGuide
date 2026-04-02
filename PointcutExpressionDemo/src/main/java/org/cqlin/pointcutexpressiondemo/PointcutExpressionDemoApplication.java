d055cbb3b768e82a969c500759a79df9bfadbf5c
Lin Changqing <cq_lin@foxmail.com>
2026-04-02 10:59:09 +0800

    feat: add a module to show the usage of point cut expression.

---
+++ PointcutExpressionDemo/src/main/java/org/cqlin/pointcutexpressiondemo/PointcutExpressionDemoApplication.java
@@ -0,0 +1,13 @@
+package org.cqlin.pointcutexpressiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointcutExpressionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointcutExpressionDemoApplication.class, args);
+    }
+
+}
---
+++ PointcutExpressionDemo/src/test/java/org/cqlin/pointcutexpressiondemo/PointcutExpressionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cqlin.pointcutexpressiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointcutExpressionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
