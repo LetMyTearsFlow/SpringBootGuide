5259c3fde4bceab06879d9351ec14266aa8235bc
Lin Changqing <cq_lin@foxmail.com>
2026-04-06 10:44:28 +0800

    feat: add JointPoint demo module.

---
+++ AopJointPointInfoDemo/src/main/java/org/cqlin/aopjointpointinfodemo/AopJointPointInfoDemoApplication.java
@@ -0,0 +1,13 @@
+package org.cqlin.aopjointpointinfodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopJointPointInfoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopJointPointInfoDemoApplication.class, args);
+    }
+
+}
---
+++ AopJointPointInfoDemo/src/test/java/org/cqlin/aopjointpointinfodemo/AopJointPointInfoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cqlin.aopjointpointinfodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopJointPointInfoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
