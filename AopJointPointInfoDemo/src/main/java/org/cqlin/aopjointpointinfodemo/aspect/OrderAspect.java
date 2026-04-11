46953abc954e0eded8aa0bca60cd88972c91e874
Lin Changqing <cq_lin@foxmail.com>
2026-04-11 10:52:16 +0800

    feat: finish JointPoint demo module.
    This module demonstrate the usage of Around aspect method.

--- AopJointPointInfoDemo/src/main/java/org/cqlin/aopjointpointinfodemo/aspect/OrderAspect.java
+++ AopJointPointInfoDemo/src/main/java/org/cqlin/aopjointpointinfodemo/aspect/OrderAspect.java
@@ -1,5 +1,7 @@
 package org.cqlin.aopjointpointinfodemo.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.springframework.stereotype.Component;
 import org.aspectj.lang.annotation.Aspect;
 
@@ -7,4 +9,26 @@ import org.aspectj.lang.annotation.Aspect;
 @Aspect
 @Component
 public class OrderAspect {
+
+    @Around("execution(* org.cqlin.aopjointpointinfodemo.service..*.*(..))")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        String methodName = joinPoint.getSignature().getName();
+        // 前置通知
+        System.out.println("[OrderAspect] Before " + methodName);
+        // execute target method
+        Object proceed;
+        try {
+            Object[] args = joinPoint.getArgs();
+            proceed = joinPoint.proceed(args);
+        } catch (Throwable throwable) {
+            // 异常逻辑
+            System.out.println("[OrderAspect] Exception " + methodName + ": " + throwable.getMessage());
+            throw throwable;
+        } finally {
+            // 后置逻辑
+            System.out.println("[OrderAspect] Return " + methodName);
+        }
+
+        return proceed;
+    }
 }
--- AopJointPointInfoDemo/src/test/java/org/cqlin/aopjointpointinfodemo/AopJointPointInfoDemoApplicationTests.java
+++ AopJointPointInfoDemo/src/test/java/org/cqlin/aopjointpointinfodemo/AopJointPointInfoDemoApplicationTests.java
@@ -1,13 +1,21 @@
 package org.cqlin.aopjointpointinfodemo;
 
+import org.cqlin.aopjointpointinfodemo.service.OrderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AopJointPointInfoDemoApplicationTests {
 
+    @Autowired
+    public OrderService orderService;
     @Test
-    void contextLoads() {
+    void testOrderAspect() {
+        orderService.createOrder("user1", "product1");
+        orderService.queryOrder(0L);
+        assertThrows(IllegalArgumentException.class,() -> orderService.cancelOrder("dog",1L));
     }
 
 }
