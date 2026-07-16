22ae90010e3b930167956d98f3e954dec8eef428
Lin Changqing <cq_lin@foxmail.com>
2026-07-16 09:44:10 +0800

    test: user dao test

---
+++ MarketDemo/src/test/java/org/cqlin/marketdemo/dao/UserDaoTest.java
@@ -0,0 +1,38 @@
+package org.cqlin.marketdemo.dao;
+
+import org.cqlin.marketdemo.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserDaoTest {
+
+    @Autowired
+    public UserDao userDao;
+
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setUsername("user0721");
+        user.setPhone("1145141919810");
+        user.setPassword("passw0de");
+        userDao.createUser(user);
+    }
+
+    @Test
+    public void testFindUserById() {
+        User user = userDao.findUserById(1L);
+        System.out.println("username: " + user.username);
+        Assertions.assertThrows(RuntimeException.class, () -> userDao.findUserById(1008L));
+    }
+
+    @Test
+    void updateUser() {
+        User user = new User();
+        user.setPassword("123456");
+        user.setId(1L);
+        userDao.updateUser(user);
+    }
+}
