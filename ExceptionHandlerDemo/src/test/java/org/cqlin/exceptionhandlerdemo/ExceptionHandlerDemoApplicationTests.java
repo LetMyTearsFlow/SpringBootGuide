1e7d431b797ac91869e32db94e692be8dae81b42
Lin Changqing <cq_lin@foxmail.com>
2026-07-21 08:48:58 +0800

    feat: exception handler

---
+++ ExceptionHandlerDemo/src/test/java/org/cqlin/exceptionhandlerdemo/ExceptionHandlerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cqlin.exceptionhandlerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
